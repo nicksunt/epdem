533ab0a3500076b843bd6a18b2c8f22cda72013a
Nick Sunt <nicksunt85@gmail.com>
2021-12-02 00:27:36 +0300

    fix fail-4

--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -16,13 +16,13 @@ public class MyCalculatorTest {
     @Test
     public void isSum2(){
         double x = 5;
-        int y = 5;
+        int y = 4;
         double z = x+y;
         Assert.assertEquals(z,myCalculator.sum(x,y), 0.00001);
     }
     @Test
     public void isSum3(){
-        char x = 6;
+        char x = 7;
         int y = 5;
         int z = x+y;
         Assert.assertEquals(z,myCalculator.sum(x,y), 0.00001);
